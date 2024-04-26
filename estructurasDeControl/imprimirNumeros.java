606dff7920fd28a9936b3a9c3a5ee6401b9786c3
KevDCS <kcruzs@outlook.com>
2024-04-26 11:58:17 +0200

    Se hizo el código con un ciclo for para que se impriman los números del 1 al 10.

---
+++ estructurasDeControl/imprimirNumeros.java
@@ -0,0 +1,13 @@
+// Implementa un bucle `for` que imprima números del 1 al 10.
+
+package estructurasDeControl;
+
+public class imprimirNumeros {
+  
+  public static void main(String[] args) {
+
+    for (int i = 1; i<= 10; i++) {
+      System.out.println(i);
+    }
+  }
+}
